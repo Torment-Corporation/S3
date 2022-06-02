d5f8f15f0e8c65783ff72274a52488ab6ae7e35d
xnudd <natdarnai.dev@gmail.com>
2022-06-03 03:19:10 +0700

    section 1 success

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,26 @@
 const socket = io();
+const tweetStream = document.getElementById("tweetStream");
+const tweet = [];
 socket.on("connect", () => {
   console.log("Connected to Server...");
 });
 socket.on("tweet", (tweet) => {
   console.log(tweet);
+  const tweetData = {
+    id: tweet.data.id,
+    text: tweet.data.text,
+    source: tweet.data.source,
+    username: `@${tweet.includes.users[0].username}`,
+  };
+  const tweetEl = document.createElement("div");
+  tweetEl.className = "card my-4";
+  tweetEl.innerHTML = `
+    <div class="card-body">
+      <p>${tweetData.text}</p>
+      <p>${tweetData.source}</p>
+      <p>${tweetData.username}</p>
+      <a class="btn btn-primary mt-3" target="_blank" href="https://twitter.com/${tweetData.username}/status/${tweetData.id}">Go</a>
+    </div>
+  `;
+  tweetStream.appendChild(tweetEl);
 });
