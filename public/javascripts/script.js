869f91c139f102364573242a3ac41765fc410d28
xnudd <natdarnai.dev@gmail.com>
2022-06-03 04:49:58 +0700

    +

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -11,14 +11,16 @@ socket.on("tweet", (tweet) => {
     text: tweet.data.text,
     source: tweet.data.source,
     username: `@${tweet.includes.users[0].username}`,
+    img_profile: `${tweet.includes.users[0].profile_image_url}`,
   };
   const tweetEl = document.createElement("div");
-  tweetEl.className = "card my-4";
+  tweetEl.className = "card bg-dark my-4";
   tweetEl.innerHTML = `
-    <div class="card-body">
+    <div class="card-body text-light">
       <p>${tweetData.text}</p>
       <p>${tweetData.source}</p>
       <p>${tweetData.username}</p>
+      <img src="${tweetData.img_profile}" class="rounded-circle">
       <a class="btn btn-primary mt-3" target="_blank" href="https://twitter.com/${tweetData.username}/status/${tweetData.id}">Go</a>
     </div>
   `;
